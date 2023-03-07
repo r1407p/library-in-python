cdfbc21e2ab0dd24122180c9a4a54490d9abd3a7
r1407p <buszter0403@gmail.com>
2023-03-07 22:52:47 +0800

    add numpy

--- Numpy/create_array.py
+++ Numpy/create_array.py
@@ -13,14 +13,41 @@ print(np.ones(12))
 #[1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1.]
 print(np.empty(3).size)
 #np.empty create an array but dont assign  value
-print(np.arange(10))
+print(np.arange(10,dtype= float))
 #[0 1 2 3 4 5 6 7 8 9]
+print(np.arange(10,20))
+#[10 11 12 13 14 15 16 17 18 19]
 print(np.arange(10,20,3))
 #[10 13 16 19]
+print(np.linspace(0,10,5))
+#[ 0.   2.5  5.   7.5 10. ]
+print(np.asarray(A)) # copy
+#[1 2 3]
+print(np.ones_like(A)) 
+#create an nd array same as a but value are all one
+#[1 1 1] 
+print(np.zeros_like(A)) 
+#create an nd array same as a but value are all zero
+#[0 0 0] 
+print(np.empty(A)) 
+#create an nd array same as a but value are all empty
+print(np.full(5,8))
+#[8 8 8 8 8]
+print(np.random.random((2,3)))
+#[[0.57281141 0.98874283 0.44597598]
+ #[0.0412086  0.64839552 0.04160949]]
+print("+++++++++++++++++++")
+
 
 """ two dimension """
 A = np.array([[1, 2, 3], [4, 5, 6]])
 B = np.array([[7, 8, 9], [1, 2, 3]])
+print(A.shape)
+#(2,3)
+print(A.size)
+#6
+print(A.sum())
+
 print(np.empty([3,2]))
 #[[0. 0.]
 #[0. 0.]
@@ -29,6 +56,12 @@ print(np.zeros([2,3]))
 print(np.ones([2,3]))
 #[[0. 0. 0.]
 # [0. 0. 0.]]
+print(np.eye(3))
+#print(np.identity(3))
+#[[1. 0. 0.]
+# [0. 1. 0.]
+# [0. 0. 1.]]
+
 
 """ three dimension"""
 print(np.array([
@@ -49,4 +82,4 @@ print(np.array([
         [2,2]   
     ]
 ]))
-print(np.array([2,1,1,2]))
+print(np.ones([2,1,1,2]))
---
+++ Numpy/operation.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+A = np.array([3,6,9])
+B = np.array([2,7,5])
+"""element wise"""
+print(A+B)
+#[5 12 14 ]
+print(A-B)
+#[1 -1 4 ]
+print(A*B)
+#[ 6 42 45]
+print(A/B)
+#[1.5        0.85714286 1.8       ]
+print(A>B)
+#[ True False  True]
+print(A<B)
+#[False  True False]
+print(A==B)
+#[False False False]
+"""-----------"""
+
+
+
+
+
+
+
+
